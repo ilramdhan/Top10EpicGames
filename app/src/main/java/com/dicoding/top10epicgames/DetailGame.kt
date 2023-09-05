e09481a802cac15985273526a6aa13b0c74d8009
Ilham Ramadhan <ilhamram332@gmail.com>
2023-09-05 10:36:16 +0700

    done

--- app/src/main/java/com/dicoding/top10epicgames/DetailGame.kt
+++ app/src/main/java/com/dicoding/top10epicgames/DetailGame.kt
@@ -1,6 +1,8 @@
 package com.dicoding.top10epicgames
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -36,4 +38,20 @@ class DetailGameActivity : AppCompatActivity() {
             imgGame.setImageResource(gameImageResId)
         }
     }
+
+    fun shareGame(view: View) {
+        val gameName = findViewById<TextView>(R.id.tv_game_name).text.toString()
+        val gameDescription = findViewById<TextView>(R.id.tv_game_description).text.toString()
+
+        val shareText = "Check out this awesome game:\n\n$gameName\n\n$gameDescription"
+
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+        sendIntent.type = "text/plain"
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
 }
